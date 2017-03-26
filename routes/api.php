803fda75a0621c2d15aa432bd7fcd89b779e1002
adikso <adam@zambrzycki.net>
2017-03-26 22:50:51 +0200

    Fix: ea11202. Not pushed change

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,8 @@
 <?php
+Router::add(new Route('cron::send::raport', '/cron/raport/{level}/{target}', [
+    "GET" => "CronController::send"
+], Roles::Guest));
+
 Router::add(new Route('cron::send::raport', '/cron/raport/{level}', [
     "GET" => "CronController::send"
 ], Roles::Guest));
